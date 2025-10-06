32c917e75cd0df90e7c30ba95ae54417408673b6
minkyo <smk78449@naver.com>
2025-10-07 00:53:25 +0900

    feat: ReservationView UI

---
+++ mango/Megabox/Megabox/Features/ReservationView.swift
@@ -0,0 +1,14 @@
+//
+//  ReservationView.swift
+//  Megabox
+//
+//  Created by 송민교 on 10/4/25.
+//
+
+import SwiftUI
+
+struct ReservationView: View {
+    var body: some View {
+        Text("ReservationView")
+    }
+}
